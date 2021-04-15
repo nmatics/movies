eee4391110bf84eac1787ad1310420de159441d7
Nick Matics <nickoi@hotmail.com>
2021-04-15 14:08:22 -0400

    completed knex.js learn module

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const knex = require("knex")(require("./knexfile.js")[process.env.NODE_ENV]);
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+app.get("/movies", function (req, res) {
+  knex
+    .select("*")
+    .from("movies")
+    .where({ title: "Orfeu Negro" })
+    .then((data) => res.status(200).json(data))
+    .catch((err) =>
+      res.status(404).json({
+        message:
+          "The data you are looking for could not be found. Please try again",
+      })
+    );
+});
+
+app.listen(PORT, () => {
+  console.log(`The server is running on ${PORT}`);
+});
